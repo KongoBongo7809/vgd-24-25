ec0f1e2945288fab6a5e5b1c9ad0097e5c45be01
KongoBongo7809 <147113188+KongoBongo7809@users.noreply.github.com>
2024-09-30 14:29:58 -0400

    Update Player.cs
    DON'T MERGE THE CODE, just working on this during school. Will cause errors if merged.

--- TSA VGD 2025/Assets/Scripts/Player.cs
+++ TSA VGD 2025/Assets/Scripts/Player.cs
@@ -9,12 +9,19 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     Vector2 movement;
     public char[] keybinds;
-
+    //Animation
+    public Animator animator;
+    
     private void Update()
     {
         //Set the movement vector based on inputs
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //Adjusting the player sprite animation based on the direction of the player
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
