4b13f3a33d326aecfca96c52be842f62ace8faec
KongoBongo7809 <iambornildas@gmail.com>
2024-09-30 21:39:09 -0400

    basic car movements
    added car movements in 2d simple direction, with animator

--- TSA VGD 2025/Assets/Scripts/Player.cs
+++ TSA VGD 2025/Assets/Scripts/Player.cs
@@ -9,12 +9,22 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     Vector2 movement;
     public char[] keybinds;
+    //Animation
+    public Animator animator;
 
     private void Update()
     {
         //Set the movement vector based on inputs
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //Set animations according to user inputs and speed
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if(animator.GetFloat("Speed") <= 0.05 ) { animator.enabled = false; }
+        else { animator.enabled = true; }
+        
     }
 
     private void FixedUpdate()
