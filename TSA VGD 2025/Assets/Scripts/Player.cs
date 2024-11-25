15dbf4e30f579fa6386a05d1b9787c1dd0fc9ef4
PIANDE314 <rudrapatel271828@gmail.com>
2024-11-25 14:49:47 -0500

    Revert "split screen"
    This reverts commit c2adf67c6dc1952da3655ab01327a2f63251e026.

--- TSA VGD 2025/Assets/Scripts/Player.cs
+++ TSA VGD 2025/Assets/Scripts/Player.cs
@@ -8,21 +8,23 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;
     public float moveSpeed = 5f;
     Vector2 movement;
-    public string color;
+    public char[] keybinds;
     //Animation
     public Animator animator;
 
     private void Update()
     {
         //Set the movement vector based on inputs
-        movement.x = Input.GetAxisRaw("Horizontal " + color);
-        movement.y = Input.GetAxisRaw("Vertical " + color);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
         //Set animations according to user inputs and speed
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-        animator.enabled = animator.GetFloat("Speed") > 0.05;
+        if(animator.GetFloat("Speed") <= 0.05 ) { animator.enabled = false; }
+        else { animator.enabled = true; }
+        
     }
 
     private void FixedUpdate()
